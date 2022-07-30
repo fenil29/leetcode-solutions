c8d2c4f0b96be5717066e0c59f2e5ad570e36e96
fenil29 <fenilkaneria@gmail.com>
2022-07-30 19:42:36 -0400

    new file:   567. Permutation in String/solution.py

---
+++ 567. Permutation in String/solution.py
@@ -0,0 +1,31 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        windowCharCount={}
+        s1CharCount={}
+        ans=False
+        for char in s1:
+            s1CharCount[char] = s1CharCount.get(char,0)+1
+        start=0
+        end=len(s1)-1
+        for char in s2[0:end+1]:
+            windowCharCount[char]= windowCharCount.get(char,0)+1
+        if(self.checkPermutation(windowCharCount,s1CharCount)):
+            ans=True
+        if(self.checkPermutation(windowCharCount,s1CharCount)):
+            ans=True        
+        while(end+1<len(s2)):
+            end+=1
+            windowCharCount[s2[end]] = windowCharCount.get(s2[end],0)+1
+            windowCharCount[s2[start]] -=1
+            start=start+1 
+            if(self.checkPermutation(windowCharCount,s1CharCount)):
+                ans=True 
+        return ans
+    
+    def checkPermutation(self,windowCharCount,s1CharCount):
+        isValid=True
+        for c in s1CharCount:
+            if (c not in windowCharCount) or s1CharCount[c]!=windowCharCount[c]:
+                isValid=False
+        return isValid
+            
