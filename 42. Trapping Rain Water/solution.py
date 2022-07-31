2779644f43db2c7476ca0a9881d15df863f006c3
fenil29 <fenilkaneria@gmail.com>
2022-07-31 17:58:22 -0400

    modified:   42. Trapping Rain Water/solution.py

--- 42. Trapping Rain Water/solution.py
+++ 42. Trapping Rain Water/solution.py
@@ -1,20 +1,18 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        leftHighest = [None] * len(height)
-        rightHighest = [None] * len(height)
-        ans = 0
-        maxValue = 0
+        leftHighest=[None] * len(height)
+        ans=0
+        maxValue=0        
         for i in range(len(height)):
-            maxValue = max(maxValue, height[i])
-            leftHighest[i] = maxValue
-        maxValue = 0
-        for i in range(len(height)-1, -1, -1):
-            maxValue = max(maxValue, height[i])
-            rightHighest[i] = maxValue
-        for i, num in enumerate(height):
-            totalCapacity = min(leftHighest[i], rightHighest[i])
-            water = totalCapacity-num
-            if water < 0:
-                water = 0
-            ans = ans+water
+            maxValue=max(maxValue,height[i])
+            leftHighest[i]=maxValue
+        maxValue=0        
+        for i in range(len(height)-1,-1,-1):
+            maxValue=max(maxValue,height[i])
+            leftHighest[i]=min(maxValue,leftHighest[i])
+        for i,num in enumerate(leftHighest):
+            water=num-height[i]
+            if water<0:
+                water=0
+            ans=ans+water
         return ans
